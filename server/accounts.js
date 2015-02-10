49a5a35ed825fbb1cadc5427283a7c5a82270545
Matt Newman <mattrobertnewman@gmail.com>
2015-02-10 09:00:34 -0700

    hard coded key now for deploying to meteor.com

--- server/accounts.js
+++ server/accounts.js
@@ -2,8 +2,10 @@ Meteor.startup(function() {
   ServiceConfiguration.configurations.remove({service: 'meteor-developer'});
   ServiceConfiguration.configurations.insert({
     service: 'meteor-developer',
-    clientId: Meteor.settings.meteorDeveloper.clientId,
-    secret: Meteor.settings.meteorDeveloper.secret
+    //clientId: Meteor.settings.meteorDeveloper.clientId,
+    //secret: Meteor.settings.meteorDeveloper.secret
+    clientId: "AF8xkkHmgaDuKot2y",
+    secret: "G5m7WnHqya2RJSgoBEPnBDMZmdkH6NsPHj"
   });
 });
 
